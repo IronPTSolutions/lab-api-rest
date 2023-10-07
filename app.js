99b191af106adc1b954f5b1d9fdfa8de8f9c425c
0000nebu <neboa.fresco@gmail.com>
2023-10-07 11:50:56 +0200

    chore: app and port

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+const port = process.env.port || 3000;
+
+app.listen(port, () => console.info(`App listen in port ${port}`))
